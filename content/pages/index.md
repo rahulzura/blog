4648ee0a57ef698f3370a558fc3ade8c64240dbb
rahulzura <rahuldesune@gmail.com>
2020-02-07 16:07:07 +0530

    added projects link

--- content/pages/index.md
+++ content/pages/index.md
@@ -13,10 +13,55 @@ js: "index.js"
 	<div class="intro__dp">
 	</div>
 	<div class="intro__text">
-		<p><strong>Hey, I'm Rahul</strong>. I'm a full stack web developer, based in India. Below are some projects and articles by me.</p>
+		<h1>Hey, I'm Rahul.</h1>
+		<p>I'm a full stack software developer, based in India. Below are some projects and articles by me.</p>
 	</div>
-	<div class="clear"></div>
 </div>
-# Latest Articles
 
-- [MySQL in Ubuntu and Debian-based Linux](./mysql-in-ubuntu-and-debian-based-linux.html)
+<div class="everything-else">
+  <h1>Latest Articles</h1>
+	<ul>
+		<li><a href="./mysql-in-ubuntu-and-debian-based-linux.html">MySQL in Ubuntu and Debian-based Linux</a></li>
+	</ul>
+	<h1 id="projects">Projects</h1>
+	<div class="project-list clear">
+		<div class="project">
+			<div class="project__img">
+				<img src="code-n-share.png" alt="Code N Share Image"/>
+			</div>
+			<div class="project__heading">
+				Code N Share
+			</div>
+			<div class="project__description">
+				Online HTML and CSS editor. Gives a link of the page build with provided HTML and CSS which can be shared, the link is valid for an hour.
+			</div>
+			<div class="project__links">
+				<div class="link-icon-div">
+					<a href="https://github.com/rahulzura/code-n-share">Github</a>
+				</div>
+				<div class="link-icon-div">
+					<a href="https://rahulzura.github.io/code-n-share">Launch</a>
+				</div>
+			</div>
+		</div>
+		<div class="project">
+			<div class="project__img">
+				<img src="face-detector.png" alt="Face Detector Image" />
+			</div>
+			<div class="project__heading">
+				Face Detector
+			</div>
+			<div class="project__description">
+				This app detects any faces in an image, built using React for UI and Node.js with Express.js for backend. It uses Clarifai Face Detection API to detect faces in the images. It is deployed on Heroku.
+			</div>
+			<div class="project__links">
+				<div class="link-icon-div">
+					<a href="https://github.com/rahulzura/face-detector">Github</a>
+				</div>
+				<div class="link-icon-div">
+					<a href="https://face-square.herokuapp.com">Launch</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- cssjs/js/index.js
+++ cssjs/js/index.js
@@ -1,4 +1,9 @@
 window.onload = () => {
+  loadDp();
+};
+
+// Load dp
+const loadDp = () => {
   const introDpDiv = document.querySelector(".intro__dp");
 
   const img = document.createElement("img");
@@ -6,3 +11,86 @@ window.onload = () => {
   img.setAttribute("alt", "My display picture");
   introDpDiv.appendChild(img);
 };
+
+// Project List
+const projectList = [
+  {
+    thumbnail: "face-detector.jpg",
+    title: "Face Detector",
+    des:
+      "This app detects any faces in an image, built using React for UI and Node.js with Express.js for backend. It uses Clarifai Face Detection API to detect faces in the images. It is deployed on Heroku.",
+    github: "https://github.com/rahulzura/face-detector",
+    live: "https://face-square.herokuapp.com/"
+  },
+  {
+    thumbnail: "code-n-share.jpg",
+    title: "Code N Share",
+    des:
+      "Online HTML and CSS editor. Gives a link of the page build with provided HTML and CSS which can be shared, the link is valid for an hour.",
+    github: "https://github.com/rahulzura/code-n-share",
+    live: "https://github.com/rahulzura/code-n-share-backend"
+  }
+];
+
+// Load Project List
+const loadProjectList = projectList => {
+  const projectListDiv = document.querySelector(".project-list");
+
+  const projectElements = projectList.map(project => {
+    const divProject = document.createElement("div");
+    divProject.setAttribute("class", "project");
+
+    // 3. to append to divProject
+    const divThubmnail = document.createElement("div");
+    divThubmnail.setAttribute("class", "thumbnail");
+
+    // 1. to append to divThumbnail
+    const img = document.createElement("img");
+    img.setAttribute("src", project.thumbnail);
+    img.setAttribute("alt", project.title);
+
+    // 4. to append to divProject
+    const divDes = document.createElement("div");
+    divDes.setAttribute("class", "des");
+
+    // 2. to append to divDes
+    const text = document.createTextNode(project.des);
+
+    // 5. to append to divProject
+    const divClear = document.createElement("div");
+    divClear.setAttribute("class", "clear");
+
+    const divGithub = document.createElement("div");
+    divGithub.setPointerCapture("class", "project__link");
+
+    const divLaunch = document.createElement("div");
+    divLaunch.setAttribute("class", "project-link");
+
+    // append in the above order
+    divThubmnail.appendChild(img);
+    divDes.appendChild(text);
+    divProject.appendChild(divThubmnail);
+    divProject.appendChild(divDes);
+    divProject.appendChild(divClear);
+
+    // return divProject
+    return divProject;
+  });
+
+  // append projectElements to projectListDiv
+  projectElements.forEach(projectElement =>
+    projectListDiv.appendChild(projectElement)
+  );
+};
+
+{
+  /* <div class="intro">
+    <div class="intro__dp">
+      <img src="dp.jpg" alt="My display picture">
+    </div>
+    <div class="intro__text">
+        <p><strong>Hey, I'm Rahul</strong>. I'm a full stack web developer, based in India. Below are some projects and articles by me.</p>
+    </div>
+    <div class="clear"></div>
+</div> */
+}
