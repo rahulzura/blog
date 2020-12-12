3a468a4d2706873f2952b2e08b5cfe07a3065d68
Rahul Kumar <rahulkay3@gmail.com>
2020-12-12 18:35:36 +0530

    dp.jpg to images/db.jpg by js

--- cssjs/js/index.js
+++ cssjs/js/index.js
@@ -7,7 +7,7 @@ const loadDp = () => {
   const introDpDiv = document.querySelector(".intro__dp");
 
   const img = document.createElement("img");
-  img.setAttribute("src", "dp.jpg");
+  img.setAttribute("src", "images/dp.jpg");
   img.setAttribute("alt", "My display picture");
   introDpDiv.appendChild(img);
 };
